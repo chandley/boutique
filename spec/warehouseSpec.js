8aea89ef088ba1b9fafb51762677687fb97feae9
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-02-24 13:58:39 +0000

    warehouse can add and remove items

---
+++ spec/warehouseSpec.js
@@ -0,0 +1,28 @@
+var Warehouse = require('../src/warehouse')
+
+describe("Warehouse", function() {
+  
+  var warehouse;  
+
+  beforeEach(function() {
+    warehouse = new Warehouse()
+  });
+
+  it("starts off empty", function() {
+    expect(warehouse.stock).toEqual([]);
+  });
+  
+  it("can add an item", function() {
+    warehouse.add('item')
+    expect(warehouse.stock).toEqual(['item']);  
+  })
+
+  it("can remove an item", function() {
+    warehouse.add('item')
+    warehouse.remove('item')
+    expect(warehouse.stock).toEqual([]);
+  });
+  
+  
+});  
+
---
+++ src/warehouse.js
@@ -0,0 +1,16 @@
+function Warehouse () {
+  this.stock = []; 
+}
+
+module.exports = Warehouse;
+
+Warehouse.prototype.add =
+function(item) {
+  this.stock.push(item);
+};
+
+Warehouse.prototype.remove =
+function(item) {
+  index = this.stock.indexOf(item);
+  this.stock.splice(index, 1);
+};
